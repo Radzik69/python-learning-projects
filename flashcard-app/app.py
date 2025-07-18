03f9d78efff83e36b4735e95566fe9f044aac34e
Radzik69 <126457866+Radzik69@users.noreply.github.com>
2025-07-18 14:23:35 +0200

    flashcard app with GUI done

--- flashcard-app/app.py
+++ flashcard-app/app.py
@@ -1,6 +1,7 @@
 import requests
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 
 class FlashCardApiResponse:
     def __init__(self):
@@ -68,7 +69,7 @@ class FlashCardApiResponse:
             f"https://opentdb.com/api.php?amount=1&category={category}&difficulty={difficulty}&type=multiple"
         )
         data = response.json()
-        print(data)
+        return data
 
 class CreateGUI:
     def __init__(self):
@@ -104,30 +105,37 @@ class CreateGUI:
         self.difficulty_chosen.pack()
         self.difficulty_chosen.current(0)
 
-        self.button = tk.Button(
+        self.start_button = tk.Button(
             self.root,
-            command=self.start_flashcard_questions,  # no ()
+            command=self.start_flashcard_questions,
             text="Start with options above",
             activebackground="red",
             activeforeground="lightgrey",
             cursor="arrow",
         )
-        self.button.pack(padx=20, pady=20)
+        self.start_button.pack(padx=20, pady=20)
 
         self.root.mainloop()
 
     def start_flashcard_questions(self):
         selected_category_name = self.category_chosen.get()
+        global selected_difficulty
         selected_difficulty = self.difficulty_chosen.get()
+        global category_number
+        global api_response
+        global past_questions
+        global question_number
 
-        category_number = None
         for number, name in self.api.available_categories.items():
             if name == selected_category_name:
                 category_number = number
                 break
 
         if category_number is not None:
-            self.api.get_flashcard(category_number, selected_difficulty)
+            past_questions = {}
+            api_response = self.api.get_flashcard(category_number, selected_difficulty)
+            question_number=1
+            past_questions[f"{question_number}"] = [api_response['results'][0]['question'],api_response['results'][0]['correct_answer']]
         else:
             print("Error: Category not found.")
 
@@ -135,10 +143,110 @@ class CreateGUI:
         self.label_difficulty.destroy()
         self.category_chosen.destroy()
         self.difficulty_chosen.destroy()
-        self.button.destroy()
+        self.start_button.destroy()
+
+        self.questionanswer_button = tk.Button(
+            self.root,
+            command=self.reveal_answer,
+            text=api_response['results'][0]['question'],
+            activebackground="green",
+            activeforeground="red",
+            cursor="hand2",
+            bd=40,
+            height=10,
+            width=25,
+            font=("Arial",18),
+            wraplength=200
+
+        )
+        self.questionanswer_button.pack(padx=10,pady=10)
+
+        self.buttons_frame = tk.Frame(
+            self.root,
+            bg="grey",
+            bd=10,
+            cursor="arrow",
+            height=10,
+            width=30,
+            highlightcolor="blue",
+        )
+
+        self.buttons_frame.pack(padx=10,pady=10)
+
+        self.next_question_button = tk.Button(
+            self.buttons_frame,
+            bg="lightblue",
+            height=10,
+            width=18,
+            text="Next question",
+            font=("Arial",14),
+            command=self.next_question
+        )
+
+        self.next_question_button.pack(side=tk.RIGHT,padx=10,pady=10)
+
+        self.previous_question_button = tk.Button(
+            self.buttons_frame,
+            bg="lightblue",
+            height=10,
+            width=18,
+            text="Previous question",
+            font=("Arial", 14),
+            command=self.previous_question
+        )
+
+        self.previous_question_button.pack(side=tk.LEFT,padx=10,pady=10)
+
+        self.current_question = [api_response['results'][0]['question'], api_response['results'][0]['correct_answer']]
 
 
-if __name__ == "__main__":
-    CreateGUI()
+    def reveal_answer(self):
+        self.questionanswer_button.config(text=self.current_question[1])
+        self.questionanswer_button.config(command=self.go_back_to_question)
+
+    def go_back_to_question(self):
+        self.questionanswer_button.config(text=self.current_question[0])
+        self.questionanswer_button.config(command=self.reveal_answer)
+
+    def next_question(self):
+        global api_response
+        global question_number
+        try:
+            api_response = self.api.get_flashcard(category_number, selected_difficulty)
+            self.current_question = [api_response['results'][0]['question'],
+                                     api_response['results'][0]['correct_answer']]
+            self.questionanswer_button.config(
+                text=self.current_question[0],
+                command=self.reveal_answer
+            )
+            question_number += 1
+            past_questions[question_number] = self.current_question
+        except Exception:
+            messagebox.showinfo(title='Error',
+                                message='You switched to next question too fast, and API didn\'t respond in time')
+
+    def previous_question(self):
+        global question_number
+        if question_number > 1:
+            question_number -= 1
+            prev_q = past_questions[question_number]
+            self.current_question = prev_q
+            self.questionanswer_button.config(
+                text=prev_q[0],
+                command=lambda: self.show_previous_answer(prev_q)
+            )
+        else:
+            messagebox.showinfo(title='Info', message='No previous questions.')
+
+    def show_previous_answer(self, prev_q):
+        self.questionanswer_button.config(text=prev_q[1])
+        self.questionanswer_button.config(command=lambda: self.show_previous_question(prev_q))
+
+    def show_previous_question(self, prev_q):
+        self.questionanswer_button.config(text=prev_q[0])
+        self.questionanswer_button.config(command=lambda: self.show_previous_answer(prev_q))
+
+
+CreateGUI()
 
 
